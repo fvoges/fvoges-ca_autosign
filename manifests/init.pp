4f08692f11ea9fc0f8ca76eed61cba088dc47716
Federico Voges <federico@puppetlabs.com>
2014-10-06 17:24:08 +0100

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,50 @@
+# Class: mysqlenc
+#
+#
+class cert_autosign (
+  $enable = true,
+  $dbuser = 'encrw',
+  $dbpass = 'rwsecret',
+  $dbhost = 'localhost',
+  $dbname = 'encdb',
+  $install_dir = '/opt/puppet/bin',
+  $manage_deps = false,
+) {
+  if $manage_deps {
+    package { ['gcc','mysql-devel']:
+      ensure => present,
+    }
+    package { ['mysql2','syslog_logger']:
+      ensure   => present,
+      provider => pe_gem,
+    }
+  }
+
+  file { "${install_dir}/mysql-cert-autosign.rb":
+    ensure => file,
+    source => "puppet:///${module_name}/mysql-cert-autosign.rb",
+    mode   => '0755',
+    owner  => 'pe-puppet',
+    group  => 'pe-puppet',
+    require => File["${install_dir}/mysql-cert-autosign.yaml"],
+  }
+  file { "${install_dir}/mysql-cert-autosign.yaml":
+    ensure  => file,
+    content => template("${module_name}/mysql-cert-autosign.yaml.erb"),
+    mode    => '0600',
+    owner   => 'pe-puppet',
+    group   => 'pe-puppet',
+  }
+
+
+  ini_setting { 'Puppet CA Autosign':
+    ensure  => present,
+    path    => "${confdir}/puppet.conf",
+    section => "master",
+    setting => "autosign",
+    value   => "${install_dir}/mysql-cert-autosign.rb",
+    require => File["${install_dir}/mysql-cert-autosign.rb"],
+    notify  => Service['pe-httpd'],
+  }
+
+}
